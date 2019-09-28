ef30e376896dd089e928e228251ea148830d1c8a
Eric Loudon <loudon.eric@gmail.com>
2019-09-28 15:23:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,16 @@ class App < Sinatra::Base
   get '/' do
   "Hello, World!"
  end
+
+ get '/name' do
+ "My name is Eric Loudon."
+  end
+
+ get '/hometown' do
+ "My hometown is Sydney, Australia."
+ end
+
+get '/favorite-song' do
+"My favourite song is (Sittin'On) The Dock of the Bay by Otis Redding."
+ end
 end
